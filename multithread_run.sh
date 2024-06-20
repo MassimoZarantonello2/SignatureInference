8d83387dc5aee72f024c34029607cabb30529383
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2024-06-20 14:37:55 +0200

    Aggiunta run per i campioni con le tissues

--- multithread_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export OPENBLAS_NUM_THREADS=1
-export GOTO_NUM_THREADS=1
-export OMP_NUM_THREADS=1
-source autogluon/bin/activate
-python /hdd/home/mzarantonello/stage/signature_inference/scripts/multithread_exposureInference.py
