f9b8a1083abcfdfa0d8f381d2c78fa02a8c4c0a8
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2024-09-12 18:32:26 +0200

    Rimosso lo stop della run singola

--- scripts/EvaluationsRunner.py
+++ scripts/EvaluationsRunner.py
@@ -122,7 +122,7 @@ class EvaluationsRunner:
                 json.dump(all_evaluations_df, open(self.save_evaluation_path, 'w'))
 
             # This is to stop the code after the first run
-            break
+            #break
 
 if __name__ == "__main__":
     bin_ground_truth_path = './simulations/ground_truth/bin_exposures.csv'
@@ -133,7 +133,7 @@ if __name__ == "__main__":
     sampling_values = ['1','0.9','0.8','0.7','0.6','0.5','0.4','0.3','0.2','0.15','0.1','0.05','0.04','0.03','0.02','0.01']
     run_values = [str(i) for i in range(1, 101)]
     train_test_split_value = 0.8
-    time_limit = 5
+    time_limit = None
 
     #+------------------------(1)Normal run of models-------------------------   
     #|TRAIN                          |  TEST
