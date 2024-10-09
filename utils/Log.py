3dea8531d850fb055b01c109536b72df3d7d0c6f
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2024-10-09 17:58:36 +0200

    bug fix

--- utils/Log.py
+++ utils/Log.py
@@ -1,12 +1,14 @@
+import os
+
 class LogClass:
     file_path = 'logs/'
     file_name = 'log.txt'
 
     def __init__(self, file_name):
         self.file_name = file_name
-        pass
 
-    @staticmethod
-    def log(message):
-        with open(LogClass.file_path + LogClass.file_name, 'a') as f:
+    def log(self, message):
+        if not os.path.exists(self.file_path):
+            os.makedirs(self.file_path)
+        with open(self.file_path + self.file_name, 'a+') as f:
             f.write(message + '\n')
