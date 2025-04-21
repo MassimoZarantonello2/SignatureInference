99567cae8a061ed296e57b3132b0a76465fefad0
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2025-04-21 15:53:52 -0400

    Removed the tqdm from the MultiLabelPredictor

--- scripts/EvaluationsRunner.py
+++ scripts/EvaluationsRunner.py
@@ -78,7 +78,7 @@ class EvaluationsRunner:
                 path=self.save_models_path,
                 labels=self.labels,
                 problem_types=self.problem_type,
-                verbosity=0,
+                verbosity=2,
             )
             predictor.fit(
                 train_df, 
