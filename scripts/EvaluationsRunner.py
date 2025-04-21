9dc9172e2a3acb6d1e61b5c36db995057e0467e4
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2025-04-21 13:16:44 -0400

    Refactor of Evaluation code script and

--- scripts/EvaluationsRunner.py
+++ scripts/EvaluationsRunner.py
@@ -3,12 +3,12 @@ import os
 import shutil
 import threading
 import pandas as pd
-import argparse
 import sys
 sys.path.append("./")
 
 from utils.MultiLabelPredictor import MultilabelPredictor
-from utils.ModelsHyperparameters import get_hyperparameters
+from utils.HyperparametersModels import get_hyperparameters
+from utils.EvaluationsHelper import format_model_result, create_empty_json_file
 from utils.Log import LogClass
 
 
@@ -25,8 +25,6 @@ class EvaluationsRunner:
         num_run,
         train_test_split_value,
         time_limit,
-        problem_type,
-        labels,
         save_models_path,
     ):
         self.data_path = data_path
@@ -34,9 +32,7 @@ class EvaluationsRunner:
         self.gt_type = gt_type
         self.ground_truth_path = ground_truth_path
         self.labels = None
-        self.problem_type = (
-            problem_type if problem_type is None else ["binary"] * len(labels)
-        )
+        self.problem_type = None
         self.hyperparameters = get_hyperparameters()
         self.run_values = run_values
         self.num_run = num_run
@@ -46,19 +42,9 @@ class EvaluationsRunner:
         self.save_models = False
         self.time_limit = time_limit
         self.train_test_split_value = train_test_split_value
+        self.save_models_path = save_models_path
 
-    def create_empty_json_file(self):  
-        run_dict = {}
-        for run in self.run_values:     # Create the structure of the json file
-            run_index = "run_" + run
-            sample_dict = {}
-            for sample in self.sampling_values:
-                sample_index = "sampling_" + sample
-                sample_dict[sample_index] = {}
-                run_dict[run_index] = sample_dict
-        json.dump(run_dict, open(self.save_evaluation_path, "w+"))  # Save the json file
-
-    def compute_evaluations_metrics(self, evaluation_df, sample, lc):
+    def train_and_evaluate_framework(self, evaluation_df, sample, lc):
         """
         ### Input
         - evaluation_df: The dataset that will be used to train and test the model
@@ -79,15 +65,25 @@ class EvaluationsRunner:
                     "best_model":
         """
         train_df = evaluation_df.sample(
-            frac=self.train_test_split_value, random_state=42
+            frac=self.train_test_split_value, 
+            random_state=42
         )
         test_df = evaluation_df.drop(train_df.index)
         lc.log(f"For sample {sample} the train and test dataframes are created")
         try:
-            predictor = MultilabelPredictor(
-                labels=self.labels, problem_types=self.problem_type
+            predictor = MultilabelPredictor(         #Creates the MultiLabel predictor
+                path=self.save_models_path,
+                labels=self.labels,
+                problem_types=self.problem_type,
+                verbosity=1,
             )
-            predictor.fit(train_df, time_limit=self.time_limit, hyperparameters=self.hyperparameters, presets="medium_quality", num_cpus=16 )
+            predictor.fit(
+                train_df, 
+                time_limit=self.time_limit, 
+                hyperparameters=self.hyperparameters, 
+                presets="medium_quality", 
+                fit_strategy="parallel" )
+            
             lc.log(f"For sample {sample} the models are trained")
             signature_model_info = self.format_model_result(predictor, test_df)
             lc.log(f"For sample {sample} the best models are saved")
@@ -99,7 +95,7 @@ class EvaluationsRunner:
             return None
 
 
-    def threaded_evaluation(self, run, sample, output_dict, tissues, lock, lc):
+    def multhithread_framework(self, run, sample, output_dict, lock, lc):       # Here the evaluation for a single sample are done and the output_dic is filled
         """
         ### Input
         - run: The run number
@@ -111,100 +107,75 @@ class EvaluationsRunner:
         run_sample_df = pd.read_csv(
             self.runs_path + run + self.data_path + sample + ".csv"
         )  # Create the evaluation dataframe
-        evaluation_df = pd.merge(run_sample_df, self.ground_truth_df, on="Unnamed: 0")
+        evaluation_df = pd.merge(
+            run_sample_df, 
+            self.ground_truth_df, 
+            on="Unnamed: 0")
+        
         evaluation_df.drop(columns=["Unnamed: 0"], inplace=True)
 
-        signature_model_info = self.compute_evaluations_metrics(evaluation_df, sample, lc)
+        signature_model_info = self.train_and_evaluate_framework(evaluation_df, sample, lc)     # Eva
 
         with lock:
             lc.log(f"Run {run} and sample {sample} lock aquired")
             lc.log(f"Run {run} and sample {sample} evaluation done")
-            lc.log(f"Run {run} and sample {sample} lock released")
-            lc.log("-----------------------------------")
             output_dict[sample] = signature_model_info
-
-    def run_evaluations(self):
+        lc.log(f"Run {run} and sample {sample} lock released")
+        lc.log("-----------------------------------")
+        
+    def run_evaluations(self):      # Checks which run and sample has already been evaluated and starts the sample missing or the next run
         if not os.path.exists(self.save_evaluation_path):
-            self.create_empty_json_file()
+            self.create_empty_json_file(
+                self.run_values, 
+                self.sampling_values, 
+                self.save_evaluations_path)
+            
         self.ground_truth_df = pd.read_csv(
             self.ground_truth_path
         )
 
         self.labels = self.ground_truth_df.columns[1:]
+        self.problem_type = ["binary"] * len(self.labels)
+        
         run_done = 0
         for run in self.run_values:
             run_index = "run_" + run
             signature_inference_thread = []
-            all_evaluations_df = json.load(open(self.save_evaluation_path))
+            all_evaluations_df = json.load(open(self.save_evaluation_path))     # Load the whole dataset into a dataframe
             lock = threading.Lock()
             output_dict = {}
 
             for sample in self.sampling_values:
                 sample_index = "sampling_" + sample
-                if (
-                    all_evaluations_df[run_index][sample_index] == {}
-                    or all_evaluations_df[run_index][sample_index] == None
-                ):
+                lc = LogClass(f"logs/{self.gt_type}", sample)
+                if (all_evaluations_df[run_index][sample_index] == {} or all_evaluations_df[run_index][sample_index] == None):
                     print(f"Running run {run} and sample {sample}")
-                    lc = LogClass(f"logs/{self.gt_type}", sample)
                     lc.log(f"Starting run {run} and sample {sample}")
                     t = threading.Thread(
-                        target=self.threaded_evaluation,
+                        target=self.multhithread_framework,
                         args=(run, sample, output_dict, lock, lc),
                     )
                     signature_inference_thread.append(t)
                     t.start()
                 else:
+                    lc.log(
+                        f"Run {run} and sample {sample} already evaluated, skipping"
+                    )
                     print(f"Skiping run {run} and sample {sample}")
 
-            for t in signature_inference_thread:
+            for t in signature_inference_thread:        # Wait for all the threads relative to each sample in a run to finish
                 t.join()
 
-            for key in output_dict:
+            for key in output_dict:         # Update the evaluation dictionary with the new results
                 all_evaluations_df[run_index]["sampling_" + key] = output_dict[key]
 
             if signature_inference_thread.__len__() != 0:
-                json.dump(all_evaluations_df, open(self.save_evaluation_path, "w"))
+                json.dump(all_evaluations_df, open(self.save_evaluation_path, "w"))     # Rewrite the whole dataset into a json file TODO: try and save it in a more efficient way
 
-            if self.save_models_path and os.path.exists(self.save_models_path):
+            if self.save_models_path and os.path.exists(self.save_models_path):         # Delete the models folder
                 shutil.rmtree(self.save_models_path)
 
-            if self.num_run is not None:
+            if self.num_run is not None:            # If the number of runs is set, the program will stop after the number of runs
                 run_done += 1
                 if run_done >= self.num_run:
                     break
-
-    def format_model_result(self, predictor, test_df):
-        evaluations = predictor.evaluate(test_df)
-        results = []
-        for signature in evaluations:
-            model_predictor = predictor.get_predictor(signature)
-            metrics = evaluations[signature]
-            info = model_predictor.info()
-            model_info = info["model_info"]
-
-            best_model_name = info["best_model"]
-            best_model_info = model_info[best_model_name]
-
-            ensemble_model_names = best_model_info.get("features", [])
-
-            ensemble_info = [
-                {
-                    "model_name": model,
-                    "val_score": model_info[model].get("val_score"),
-                    "fit_time": model_info[model].get("fit_time"),
-                }
-                for model in ensemble_model_names
-            ]
-
-            result = {
-                "signature": signature,
-                "best_model": best_model_name,
-                "best_model_val_score": best_model_info.get("val_score"),
-                "best_model_metrics": metrics,
-                "best_model_fit_time": best_model_info.get("fit_time"),
-                "ensemble_models": ensemble_model_names,
-                "ensemble_info": ensemble_info,
-            }
-            results.append(result)
-        return results
