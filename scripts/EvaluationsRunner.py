144e8bb53b559f2aa80c6334e704790a33b9b5dd
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2025-04-21 14:28:42 -0400

    Set the verbosity back to two

--- scripts/EvaluationsRunner.py
+++ scripts/EvaluationsRunner.py
@@ -78,7 +78,7 @@ class EvaluationsRunner:
                 path=self.save_models_path,
                 labels=self.labels,
                 problem_types=self.problem_type,
-                verbosity=1,
+                verbosity=2,
             )
             predictor.fit(
                 train_df, 
