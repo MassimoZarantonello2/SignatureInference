b7a3a8d8cfc2f357d319bd9ffdb3de2a8b569862
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2025-06-01 15:10:37 -0400

    Fix save_evaluation path in run_job_threshold.sh script

--- run_job_threshold.sh
+++ run_job_threshold.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for n in {0..9}; do
-  sbatch --job-name th$n run_job.slurm --gt_type default --threshold $n --save_evaluation default_$n --save_models default_$n --hyp_type lightgbxt --num_job 5 --time_limit 240
+  sbatch --job-name th$n run_job.slurm --gt_type default --threshold $n --save_evaluations threshold/default_$n --save_models default_$n --hyp_type lightgbxt --num_job 5 --time_limit 240
 done
