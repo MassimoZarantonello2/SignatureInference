8abd927692a167a404fc0efcbc98575a3a2797fa
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2025-06-02 18:55:10 -0400

    Add script to run jobs with random values from CSV files

---
+++ run_job_random.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for file in ../simulations/ground_truth/random_values_*.csv; do
+  filename=$(basename "$file")
+  name="${filename%.csv}"
+  sbatch --job-name "$name" run_job.slurm \
+    --gt_type "$file" \
+    --save_evaluations "random/$name" \
+    --save_models "$name" \
+    --hyp_type lightgbxt \
+    --runs 5 \
+    --time_limit 240
+done
