f63b361493b502621fe42ffbb07783c97f3fee22
m.zarantonello2@campus.unimib.it <m.zarantonello2@campus.unimib.it>
2025-06-02 19:00:34 -0400

    Fix output paths for evaluation and model saving in run_job_random.sh

--- run_job_random.sh
+++ run_job_random.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
 for file in ../simulations/ground_truth/random_values_*.csv; do
   filename=$(basename "$file")
   name="${filename%.csv}"
   sbatch --job-name "$name" run_job.slurm \
     --gt_type "$file" \
-    --save_evaluations "random/$name" \
-    --save_models "$name" \
+    --save_evaluations "./results/random/${name}_evaluations.json" \
+    --save_models "./models/${name}" \
     --hyp_type lightgbxt \
     --runs 5 \
     --time_limit 240
