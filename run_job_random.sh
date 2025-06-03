01df93a84d6768cbcd7ab3961fc7eb86dba44d14
Massimo Zarantonello <mzarant@dc01.ccbr.utoronto.ca>
2025-06-03 15:14:56 -0400

    Random runs

--- run_job_random.sh
+++ run_job_random.sh
@@ -1,5 +1,5 @@
-for file in ./simulations/ground_truth/random_values_*.csv; do
+for file in ./simulations/random_values_*.csv; do
   filename=$(basename "$file")
   name="${filename%.csv}"
-  sbatch --job-name "$name" run_job.slurm --gt_path "$file" --save_evaluations "random/${name}" --save_models "$name" --hyp_type lightgbxt --runs 5 --time_limit 240
+  sbatch --job-name "$name" run_job.slurm --gt_path "$name" --save_evaluations "random/${name}" --save_models "$name" --hyp_type lightgbxt --runs 5 --time_limit 240
 done
